3a38f84b42da3cb8876ddf6ea1570d268d8bb7b7
AzStowe <aaron.z.stowe@gmail.com>
2019-07-01 10:21:10 -0500

    refactor the blog post into a seperate component

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,52 +1,65 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import Form from '../Form'
+import BlogPost from '../BlogPost'
 
 export default class Main extends Component {
-  state = {
-    isPosting: true,
-    post: [
-      {
-        title: "My Day",
-        author: "Aaron",
-        post:
-          "Aute magna reprehenderit dolor Lorem aute cupidatat amet dolor eu laboris laborum voluptate ea reprehenderit. Sint et Lorem commodo esse et cillum eu amet veniam. Exercitation laborum duis cupidatat laboris aliqua."
-      }
-    ]
-  };
+    state = {
+        isPosting: true,
+        post: [
+            {
+                title: 'My Day',
+                author: 'Aaron',
+                post:
+                    'Aute magna reprehenderit dolor Lorem aute cupidatat amet dolor eu laboris laborum voluptate ea reprehenderit. Sint et Lorem commodo esse et cillum eu amet veniam. Exercitation laborum duis cupidatat laboris aliqua.',
+            },
+        ],
+    }
 
-  handleClick = event => {
-    this.setState({
-      isPosting: !this.state.isPosting
-    });
-  };
+    handleClick = event => {
+        this.setState({
+            isPosting: !this.state.isPosting,
+        })
+    }
 
-  handleAddPost =( {title, author, post} )=> {
-    this.setState({
-      post : [{title, author, post}, ...this.state.post]
-    })
-  }
+    handleAddPost = ({ title, author, post }) => {
+        this.setState({
+            post: [{ title, author, post }, ...this.state.post],
+        })
+    }
 
-  render() {
-    const posts = this.state.post.map((post, index) => {
-      return (
-        <li key={index}>
-          <h3>{post.title}</h3>
-          <h5>{post.author}</h5>
-          <h6>{post.post}</h6>
-        </li>
-      );
-    });
+    handleDeletePost = postIdx => {
+        const newStateArray = this.state.post.filter(
+            (elem, idx) => idx !== postIdx
+        )
 
-    return (
-      <div>
-        <header>
-          <h1>Party Blog</h1>
-        </header>
-        <section>
-        <button onClick={this.handleClick}>toggle</button>
-        {!!this.state.isPosting ? <Form handleAddPost={this.handleAddPost}/> : null}<ul>{posts}</ul>
-        </section>
-      </div>
-    );
-  }
+        this.setState({ post: newStateArray })
+    }
+
+    render() {
+        const posts = this.state.post.map((post, index) => {
+            return (
+                <BlogPost
+                    key={index}
+                    {...post}
+                    handleDeletePost={this.handleDeletePost}
+                    index={index}
+                />
+            )
+        })
+
+        return (
+            <div>
+                <header>
+                    <h1>Party Blog</h1>
+                </header>
+                <section>
+                    <button onClick={this.handleClick}>toggle</button>
+                    {!!this.state.isPosting ? (
+                        <Form handleAddPost={this.handleAddPost} />
+                    ) : null}
+                    <ul>{posts}</ul>
+                </section>
+            </div>
+        )
+    }
 }
