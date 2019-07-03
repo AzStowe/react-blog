8046b7697c23dade6432f08cec2ac6f84ee1bb8c
AzStowe <aaron.z.stowe@gmail.com>
2019-07-03 10:21:48 -0500

    add login page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 // import logo from "./logo.svg";
 import './App.css'
 import Main from './Main'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
+import Login from './Login'
 
-function App() {
-    return (
-        <div className="container">
-            <Main />
-        </div>
-    )
+class App extends Component {
+    state = {
+        isLoggedIn: false,
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Login} />
+                        <Route path="/main" component={Main} />
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
 }
 
 export default App
